962cf67e0f8a7e27a48d6db5d5d9d9cd1b33bc12
Ismael Ngoie <ismael@ngoie.com>
2026-01-19 02:18:55 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,6 +101,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17887232273"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-17887232273');
+</script>
+
+        {/* Microsoft Clarity */}
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "v3rz0a592n");
+</script>
+      </head>
       <body className={`${inter.variable} ${mono.variable} font-sans bg-slate-950 text-white antialiased`}>
         {children}
       </body>
